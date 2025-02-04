b737fa3daa12d5b08fbc9e6aa95a8ef49793d9c7
ARYAN-SHAKYA-1 <shakyaaryan567@gmail.com>
2025-02-04 15:46:18 +0545

    first push

---
+++ Index.php
@@ -0,0 +1,2 @@
+<?php include  "html/section.php"
+?>
---
+++ html/section.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <div class="text1">
+            <span class="text1_text">
+                Check Your Stocks. <br>See how you grow.
+            </span>
+        </div>
+    </section>
