03ac1277caf0a820dfef2e15c21570af46ee3db9
Arjan <arjanshakya77@gmail.com>
2025-02-26 18:20:39 +0545

    ajex added

--- ja/ajax.js
+++ ja/ajax.js
@@ -1,24 +1,29 @@
-async function getdata_g() {
-  const response = await fetch("https://www.hamropatro.com/gold", {
+async function getData() {
+  const reload = await fetch("https://www.hamropatro.com/gold", {
     method: "get",
   });
-  const data = await response.text();
-  let parasa = new DOMParser();
-  let doc = parasa.parseFromString(data, "text/html");
-  let goldPrice = doc.querySelector("ul.gold-silver li:nth-child(2)").innerHTML;
-  console.log(goldPrice);
-  document.getElementById("gold-value").innerHTML = goldPrice;
-}
-
-async function getdata_s() {
-  const response = await fetch("https://www.hamropatro.com/gold", {
-    method: "get",
-  });
-  const data = await response.text();
-  let parasa = new DOMParser();
-  let doc = parasa.parseFromString(data, "text/html");
-  let silverPrice = doc.querySelector(
+  const data = await reload.text();
+  let parser = new DOMParser();
+  let dom = parser.parseFromString(data, "text/html");
+  let goldprice = dom.querySelector("ul.gold-silver li:nth-child(2)").innerHTML;
+  let silverprice = dom.querySelector(
     "ul.gold-silver li:nth-child(6)"
   ).innerHTML;
-  document.getElementById("silver-value").innerHTML = silverPrice;
+  return {
+    goldprice,
+    silverprice,
+  };
 }
+
+async function priceg() {
+  const data = await getData();
+  document.getElementById("gold-value").innerHTML = data.goldprice;
+}
+
+async function prices() {
+  const data = await getData();
+  document.getElementById("silver-value").innerHTML = data.silverprice;
+}
+
+priceg();
+prices();
