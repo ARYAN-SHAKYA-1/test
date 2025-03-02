3da6f80acc1c2b8c235c36193a471648b660b892
ARYAN-SHAKYA-1 <shakyaaryan567@gmail.com>
2025-03-02 17:15:25 +0545

    js linked

--- html/footer.php
+++ html/footer.php
@@ -3,6 +3,7 @@
   <script src="ja/valid.js"></script>
   <script src="ja/isvalid.js"></script>
   <script src="ja/ajax.js"></script>
+  <script src="ja/item_valid.js"></script>
 
   </body>
 </html>
