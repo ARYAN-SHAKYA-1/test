62449b4099c434884b2127f190536192ef7b7008
ARYAN-SHAKYA-1 <shakyaaryan567@gmail.com>
2025-04-21 17:58:43 +0545

    sql Qurey added to create table for upload

--- sql.sql
+++ sql.sql
@@ -1,3 +1,4 @@
+-- use this to create table before using
 create table login (id int not null  AUTO_INCREMENT primary key, name varchar(100) not null, email varchar(100) not null,password varchar(100) not null);
 
 create table items( item_id int not null primary key, quantity int , id int FOREIGN KEY REFERENCE LOGIN(id));
